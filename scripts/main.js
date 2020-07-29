4abb5c805325e0374e385cd0d19973f010e3f903
Michael Baumer <meeesterbaumer@gmail.com>
2020-07-29 14:06:27 -0500

    Added input and form to DOM

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import { CriminalList } from "./criminals/criminalList.js";
 // import { OfficerList } from "./officers/OfficerList.js";
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
 import { OfficerSelect } from "./officers/officerSelect.js";
+import { NoteForm } from "./notes/NoteForm.js";
 
 CriminalList()
 // OfficerList()
 ConvictionSelect()
 OfficerSelect()
+NoteForm()
--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,5 +1,18 @@
 const contentTarget = document.querySelector(".noteFormContainer")
 
 const render = () => {
-  
+  contentTarget.innerHTML = `
+    <div class="noteFormContainer">
+      <h3>Enter a note below</h3>
+      <input type="text" id="note--title" placeholder="Enter note title" />
+      <input type="text" id="note--author" placeholder="Your name here" />
+      <textarea id="note--content" placeholder="Note text here" /></textarea>
+
+      <button id="saveNote">Save Note</button>
+    </div>
+  `
+}
+
+export const NoteForm = () => {
+  render()
 }
