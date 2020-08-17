f1aaa42f13958c7507e50d87523255922ba2bc19
Michael Baumer <meeesterbaumer@gmail.com>
2020-08-17 09:06:07 -0500

    got notes to delete

--- scripts/notes/NoteHTMLConverter.js
+++ scripts/notes/NoteHTMLConverter.js
@@ -6,6 +6,7 @@ export const NoteHTMLConverter = (noteObject, criminalObject) => {
       <div class="note--content">${  noteObject.content  }</div>
       <div class="note--author">Author: ${  noteObject.author  }</div>
       <div class="note--timestamp">Timestamp: ${  new Date(noteObject.timestamp).toLocaleDateString('en-us')  }</div>
+      <button id="deleteNote--${noteObject.id}">Delete</button>
     </section>
   `
 }
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,35 +1,52 @@
-import { getNotes, useNotes } from "./NoteDataProvider.js";
+import { getNotes, useNotes, deleteNote } from "./NoteDataProvider.js";
 import { NoteHTMLConverter } from "./NoteHTMLConverter.js";
 import { useCriminals } from "../criminals/criminalDataProvider.js";
 
 const contentTarget = document.querySelector(".noteList")
 const eventHub = document.querySelector(".container")
 
+
+
+
 const render = (noteArray) => {
   const criminals = useCriminals()
-
+  
   contentTarget.innerHTML = noteArray.reverse().map(
     (currentNote) => {
       const foundCriminal = criminals.find(
         (criminalObj) => {
           return criminalObj.id === currentNote.criminalId
         }
-      )
-      return NoteHTMLConverter(currentNote, foundCriminal)
-    }
-  ).join("")
-   
+        )
+        return NoteHTMLConverter(currentNote, foundCriminal)
+      }
+      ).join("")
+      
 }
+    
+    export const NoteList = () => {
+      getNotes()
+      .then (useNotes)
+      .then (render)
+    }
+    
+    
+eventHub.addEventListener("showNotesClicked", NoteList)
 
-export const NoteList = () => {
-  getNotes()
-  .then (useNotes)
-    .then (render)
-  }
+eventHub.addEventListener("noteStateChanged", () => {
+  const allNotes = useNotes()
+  render(allNotes)
+})
 
-  eventHub.addEventListener("showNotesClicked", NoteList)
-
-  eventHub.addEventListener("noteStateChanged", () => {
-    const allNotes = useNotes()
-    render(allNotes)
+eventHub.addEventListener("click", event => {
+  if (event.target.id.startsWith("deleteNote--")) {
+    const [prefix, id] = event.target.id.split("--")
+    deleteNote(id).then(
+      () => {
+        const updatedNotes = useNotes()
+        const criminals = useCriminals()
+        render(updatedNotes, criminals)
+      }
+      )
+    }
   })
